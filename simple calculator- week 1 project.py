628777ded174d49be9ee468584c5b1ad862b018b
Rona-A <allisonakpeneye@gmail.com>
2024-06-19 11:25:05 +0100

    Add files via upload

---
+++ simple calculator- week 1 project.py
@@ -0,0 +1,18 @@
+print ("Welcome to basic calculator program")
+print ("this calculator performs the mathematical operation between the numbers 3 and 4")
+
+#variable for the integers
+a = 3
+b = 4
+
+#variable for each mathematical operation
+add = (a+b)
+sub = (a-b)
+mul = (a*b)
+div = (a/b)
+fdiv = (a//b)
+exp = (a**b)
+mod = (a%b)
+
+#output of each variable (print)
+print (f"the addition is {add}, the subtraction is {sub}, the multiplication is {mul}, the division is {div}, the floor division is {fdiv}, the exponential is {exp}, the modulus is {mod}")      
---
+++ week 1.py
@@ -0,0 +1,33 @@
+#print and comment lesson in python
+#below is a print statement
+"""
+this is a multi line commnet
+python will ommit these when executing the code.
+thank you!
+"""
+print ("Dear Rona, welcome to python")
+print (9)
+
+#DATA TYPES IN PYTHON
+print (type(10))
+print (type(5.2))
+print (type("this is a string data type"))
+print (type(True))
+print (type(False))
+
+a = 5
+b = 10.4
+c = "this is a container holding an element"
+d = True
+print (type(a))
+print (b)
+print(c)
+print (d)
+
+
+# print multiples at once: f print and commas
+print (a, b, c, d)
+print(f"the first variable is {a} the second variable is {b} the third variable is {c}")
+print ("the first variable is:", a, "the second variable is:", b, "the third variable is:", c)
+
+#Arithmetic operators
